fe532a8d6950a23eb0502022c140457705462844
MathieuBlairon <blaironmathieu701@gmail.com>
2020-07-02 19:57:14 +0200

    Create Menu and Credit

---
+++ Assets/Scripts/New.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class New : MonoBehaviour
+{
+    public string MapToCharge;
+    /*nom de lélement à charger*/
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(MapToCharge);
+        /*Charge l'élément ce trouvent dans le fichier MapToCharge*/
+    }
+}
