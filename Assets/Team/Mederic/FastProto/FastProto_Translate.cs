400a7cd1aee235e262637905923cc55f2ec2537d
Neven Axel <neven.axel@gmail.com>
2020-07-02 14:20:06 +0200

    removed handle from FP

--- Assets/Team/Mederic/FastProto/FastProto_Translate.cs
+++ Assets/Team/Mederic/FastProto/FastProto_Translate.cs
@@ -9,14 +9,4 @@ public class FastProto_Translate : FastProto_Transform
 	{
 		transform.Translate(_direction * GetInput() * _speed * Time.deltaTime, _space);
 	}
-
-	private void OnDrawGizmos()
-	{
-		Vector3 spaceAwareDirection = ReturnSpaceAwareVector(_direction,true);
-
-		Handles.color = Color.magenta;
-
-		Handles.DrawLine(transform.position, transform.position + spaceAwareDirection);
-		Handles.ConeHandleCap(0, transform.position + spaceAwareDirection, Quaternion.LookRotation(spaceAwareDirection), 0.2f, EventType.Repaint);
-	}
 }
